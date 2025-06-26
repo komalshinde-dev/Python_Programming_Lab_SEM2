44f2f2899e18c4ecab3435475e0c0a01529f2b30
Komal <cocomoto175@gmail.com>
2025-06-26 19:22:39 +0530

    Create float_input_example.py

---
+++ basics/float_input_example.py
@@ -0,0 +1,9 @@
+rice=float(input("Enter the amount of rice:"))
+sugar=float(input("Enter the amount of sugar:"))
+
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program5.py
+Enter the amount of rice:16.7
+Enter the amount of sugar:28.5
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
