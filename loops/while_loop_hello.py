c7f22414e21c616bbbb937ce737653a0a4337801
Komal <cocomoto175@gmail.com>
2025-06-26 19:31:39 +0530

    Create while_loop_hello.py

---
+++ loops/while_loop_hello.py
@@ -0,0 +1,15 @@
+i=1
+while i<=5:
+    print("Hello World")
+    i=i+1
+print("Bye")
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program15.py
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+BYE
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
