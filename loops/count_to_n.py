2ca67619bb54a2733224adec498b02ef31b2ca2e
Komal <cocomoto175@gmail.com>
2025-06-26 19:34:04 +0530

    Create count_to_n.py

---
+++ loops/count_to_n.py
@@ -0,0 +1,21 @@
+n=int(input("Enter the no.n:"))
+i=1
+while i<=n:
+      print(i)
+      i=i+1
+print("Stop")
+
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program17.py
+Enter the no.n:10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Stop
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
