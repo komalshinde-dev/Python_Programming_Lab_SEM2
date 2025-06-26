8c4de941837b9e79a6e2d68a48b03d3e28ff6090
Komal <cocomoto175@gmail.com>
2025-06-26 19:28:09 +0530

    Create voting_eligibility.py

---
+++ basics/voting_eligibility.py
@@ -0,0 +1,12 @@
+age=int(input("Enter age:"))
+if age>=18:
+   print("Eligible for voting")
+else:
+   print("Not eligible for voting")
+   
+   
+#output  
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program11.py
+Enter age:17
+Not eligible for voting
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
