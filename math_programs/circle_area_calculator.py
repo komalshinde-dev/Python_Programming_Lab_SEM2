ab21e0bd20ed517bd8b3debf6cc3ad86a45a265d
Komal <cocomoto175@gmail.com>
2025-06-26 19:23:49 +0530

    Create circle_area_calculator.py

---
+++ math_programs/circle_area_calculator.py
@@ -0,0 +1,15 @@
+radius=float(input("Enter the radius of circle:"))
+print("radius of circle is",radius)
+pi=float(input("Enter the value of pi:"))
+print("value of pi is",pi)
+area=pi*radius*radius
+print("area of circle is:",area)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program6.py
+Enter the radius of circle:7
+radius of circle is 7.0
+Enter the value of pi:3.14
+value of pi is 3.14
+area of circle is: 153.86
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
