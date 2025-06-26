3d7b0894ed3483397842c1da9c40cdbe148f089f
Komal <cocomoto175@gmail.com>
2025-06-26 19:24:28 +0530

    Create average_marks_calculator.py

---
+++ math_programs/average_marks_calculator.py
@@ -0,0 +1,14 @@
+m1=int(input("Enter the marks of first subject:"))
+m2=int(input("Enter the marks of second subject:"))
+m3=int(input("Enter the marks of third subject:"))
+average=(m1+m2+m3)/3
+print("average of three subject is",average)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program7.py
+Enter the marks of first subject:30
+Enter the marks of second subject:30
+Enter the marks of third subject:40
+average of three subject is 33.333333333333336
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ """
+
