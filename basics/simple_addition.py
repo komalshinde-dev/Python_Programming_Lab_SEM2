a20cc7398634992c815bf8c5d3a206498c184d9b
Komal <cocomoto175@gmail.com>
2025-06-26 19:19:52 +0530

    Create simple_addition.py

---
+++ basics/simple_addition.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+c=30
+print(a+b+c)
+
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program2.py
+60
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
