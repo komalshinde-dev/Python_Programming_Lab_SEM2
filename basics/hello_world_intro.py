74f132ab9ec42cd44ca1fa22cf0b0e443349a8ac
Komal <cocomoto175@gmail.com>
2025-06-26 19:19:08 +0530

    Create hello_world_intro.py

---
+++ basics/hello_world_intro.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print("Name-Komal Shinde")
+print("Branch-AIDS")
+print("Roll no-57")
+
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program1.py
+Hello World
+Name-Komal Shinde
+Branch-AIDS
+Roll no-57
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
+
+
