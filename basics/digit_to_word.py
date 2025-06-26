ded21953238fa58382b0152f2a1203a9c019319f
Komal <cocomoto175@gmail.com>
2025-06-26 19:30:03 +0530

    Create digit_to_word.py

---
+++ basics/digit_to_word.py
@@ -0,0 +1,24 @@
+a = int(input("Enter a number (0–9): "))
+if a==0:
+   print("ZERO")
+elif a==1:
+   print("ONE")
+elif a==2:
+   print("TWO")
+elif a==3:
+   print("THREE")
+elif a==4:
+   print("FOUR")
+elif a==5:
+   print("FIVE")
+elif a==6:
+   print("SIX")
+elif a==7:
+   print("SEVEN")
+elif a==8:
+   print("EIGHT")
+elif a==9:
+   print("NINE")
+else:
+   print("Not identified")  
+   
