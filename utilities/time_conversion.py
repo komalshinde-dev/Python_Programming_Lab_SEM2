f1ace541b7caa3b974740ab52e119d9d2eb60b8c
Komal <cocomoto175@gmail.com>
2025-06-26 19:26:04 +0530

    Create time_conversion.py

---
+++ utilities/time_conversion.py
@@ -0,0 +1,13 @@
+hour=int(input("enter the hours:"))
+min=hour*60
+sec=min*60
+print("enter the minutes",min)
+print("enter the seconds",sec)
+
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program8.py
+enter the hours:3
+enter the minutes 180
+enter the seconds 10800
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
