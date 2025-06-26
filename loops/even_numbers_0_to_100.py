c9fdd4da4477247f3eff56da2cc7df3cdb0b821c
Komal <cocomoto175@gmail.com>
2025-06-26 19:36:07 +0530

    Create even_numbers_0_to_100.py

---
+++ loops/even_numbers_0_to_100.py
@@ -0,0 +1,10 @@
+i = 0
+while i <= 100:
+    if i % 2 == 0:
+        print(i, end=",")
+    i+=1
+    
+#output    
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program20.py
+0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
