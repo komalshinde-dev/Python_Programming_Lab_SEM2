53829fc73613270aad705da55fac63cd109c4fca
Komal <cocomoto175@gmail.com>
2025-06-26 19:30:41 +0530

    Create even_odd_checker.py

---
+++ basics/even_odd_checker.py
@@ -0,0 +1,11 @@
+num=int(input("Enter the number:"))
+if(num%2)==0:
+  print("num is even",num)
+else:
+  print("num is odd",num)
+  
+#output  
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program14.py
+Enter the number:5
+num is odd 5
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
