680c7fd3c6cc4fcb11b513d6332f782cd149b2ee
Komal <cocomoto175@gmail.com>
2025-06-26 19:26:52 +0530

    Create discount_calculator.py

---
+++ math_programs/discount_calculator.py
@@ -0,0 +1,11 @@
+BA=int(input("Enter the bill amount:"))
+D=int(input("Enter the discount:"))
+DA=(BA*D)/100
+print("discounted amount is",DA)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program9.py
+Enter the bill amount:20000
+Enter the discount:25
+discounted amount is 5000.0
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ """
