2a6760b62a4baee6a8c3c9c05f6c75e9c40a19c4
Komal <cocomoto175@gmail.com>
2025-06-26 19:34:40 +0530

    Create sum_natural_numbers.py

---
+++ loops/sum_natural_numbers.py
@@ -0,0 +1,12 @@
+num=10
+sum=0
+for i in range(num+1):
+    sum+=i
+print(sum)
+
+
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program18.py
+55
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
