3519fed965f7bc62a40bd851eca97707aae7b506
Komal <cocomoto175@gmail.com>
2025-06-26 19:20:52 +0530

    Create user_input_info.py

---
+++ basics/user_input_info.py
@@ -0,0 +1,15 @@
+name=input("Enter your name:")
+print("your name is",name)
+branch=input("Enter your branch:")
+print("your branch is",branch)
+roll.no=input("Enter your roll.no:")
+print("your roll.no is",roll.no)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program3.py
+Enter your name:Komal Shinde
+your name is Komal Shinde
+Enter your branch:AI and DS
+your branch is AI and DS
+Enter your roll.no:57
+your roll.no is 57"""
