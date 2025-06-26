a6bef80248a9e486cff6fb1b3ad033a23c29d693
Komal <cocomoto175@gmail.com>
2025-06-26 19:27:30 +0530

    Create if_else_example.py

---
+++ basics/if_else_example.py
@@ -0,0 +1,14 @@
+if 20>10:
+   print("Biggest no is 20")
+   print("Bye Bye")
+else:
+   print("Bigeest no is 10")
+print("python")
+
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program10.py
+Biggest no is 20
+Bye Bye
+python
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
