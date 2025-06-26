d1cda2a60d3e909234169cb6fe8847b4fa93e557
Komal <cocomoto175@gmail.com>
2025-06-26 19:21:50 +0530

    Create input_addition.py

---
+++ basics/input_addition.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+sum=num1+num2+num3
+print("addition is:",sum)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program4.py
+Enter first number:10
+Enter second number:10
+Enter third number:10
+addition is: 30
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$"""
+
