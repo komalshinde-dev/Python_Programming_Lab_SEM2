5b0bfa4da709eaed920726cf13ed71a950246868
Komal <cocomoto175@gmail.com>
2025-06-28 19:26:20 +0530

    Create first_last_two_chars.py

---
+++ string_programs/first_last_two_chars.py
@@ -0,0 +1,12 @@
+str=input("enter the string:-")
+print(str[ :2])
+print(str[-2: ])
+
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program27.1.py
+enter the string:-python
+py
+on
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
+
