c000e80a5436266f833c08c2a6f73941a0f58543
Komal <cocomoto175@gmail.com>
2025-06-28 19:29:23 +0530

    Create palindrome_checker.py

---
+++ string_programs/palindrome_checker.py
@@ -0,0 +1,16 @@
+str=input("Enter a name: ")
+if (str[ : :1]==str[ : :-1]):
+   print("string is palindrome")
+else:
+   print("it is not a palindrome")
+   
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program29.py
+Enter a name: anna
+string is palindrome
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program29.py
+Enter a name: komal
+it is not a palindrome
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
+
