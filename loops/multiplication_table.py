8bb717c5d1e5447bb4c7c92477ed143aeec95b26
Komal <cocomoto175@gmail.com>
2025-06-28 19:20:50 +0530

    Create multiplication_table.py

---
+++ loops/multiplication_table.py
@@ -0,0 +1,24 @@
+num = int(input("Enter the number: "))
+
+print("Multiplication Table of", num)
+for i in range(1, 11):
+   print(num,"X",i,"=",num * i) 
+   
+   
+   
+#output    
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program22.py
+Enter the number: 8
+Multiplication Table of 8
+8 X 1 = 8
+8 X 2 = 16
+8 X 3 = 24
+8 X 4 = 32
+8 X 5 = 40
+8 X 6 = 48
+8 X 7 = 56
+8 X 8 = 64
+8 X 9 = 72
+8 X 10 = 80
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
