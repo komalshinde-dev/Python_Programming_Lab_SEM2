3d5984bdae078bb9de0d99aa1321212de01585fd
Komal <cocomoto175@gmail.com>
2025-06-28 19:28:32 +0530

    Create incremental_number_triangle.py

---
+++ pattern_programs/incremental_number_triangle.py
@@ -0,0 +1,18 @@
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print('')
+    
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program28.py
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
+
+
+   
