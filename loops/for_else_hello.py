b06b77e618224528cff4f2d6776904284d7158d9
Komal <cocomoto175@gmail.com>
2025-06-28 19:20:01 +0530

    Create for_else_hello.py

---
+++ loops/for_else_hello.py
@@ -0,0 +1,16 @@
+for x in range(5):
+    print("Hello world!") # prints "Hello, world!" 5 times
+else:
+    print("\nDone!")
+    
+#output    
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program21.py
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+
+Done!
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
