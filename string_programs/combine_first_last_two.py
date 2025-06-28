e784e9bac80e13e5953ec103a75f9b02d714b1d3
Komal <cocomoto175@gmail.com>
2025-06-28 19:26:54 +0530

    Create combine_first_last_two.py

---
+++ string_programs/combine_first_last_two.py
@@ -0,0 +1,18 @@
+str=input("Enter the string: ")
+if len(str)<3:
+  print("Please enter a valid string")
+  print("String length should be greater than 3")
+else:
+  print(str[ :2]+str[-2: ])  
+  
+  
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program27.2.py
+Enter the string: ab
+Please enter a valid string
+String length should be greater than 3
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$""" 
+
+
+
+
+
