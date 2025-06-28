d57858695c7995361700cb3b2f0025a5303c4545
Komal <cocomoto175@gmail.com>
2025-06-28 19:35:55 +0530

    Create list_stats.py

---
+++ list_operations/list_stats.py
@@ -0,0 +1,21 @@
+a = [10,20,30,40,50]
+ 
+ 
+count = sum(a)
+avg = count/len(a)
+minimum=min(a)
+maximum=max(a)
+
+print("sum = ", count)
+print("average = ", avg)
+print("min=",minimum)
+print("max=",maximum)
+
+#output
+"""pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ python3 program35.py
+sum =  150
+average =  30.0
+min= 10
+max= 50
+pl-ii@plii-HP-Pro-Tower-280-G9-PCI-Desktop-PC:~/B3_57/SEM II$ """
+
